253975708d33687eaf5321995b6ac513adefa105
ezra <ezra.mizrahi@hey.com>
2024-02-01 20:18:09 +1100

    rm

--- app/api/segment.py
+++ app/api/segment.py
@@ -1,8 +1,7 @@
 from fastapi import APIRouter, HTTPException, Form, UploadFile
 from ..image_processing.detr_resnet_101 import detectObjects
 from ..image_processing.segment_images import overlay_with_mask, extract_and_save_obj
-from ..utils.ffmpeg_utils import take_screenshot, get_movie_duration
-from ..utils.validation import validate_image, validate_video
+from ..utils.validation import validate_image
 from PIL import Image
 from io import BytesIO
 import random
@@ -44,41 +43,6 @@ async def extract_obj_with_label(file: UploadFile, label: str = Form(...)):
             "detr_output": detr_output
         }
     raise HTTPException(status_code=404, detail=f"No objects of type '{label}' detected")
-
-@router.post("/segment/extract_obj_from_video")
-async def extract_obj_from_video(file: UploadFile):    
-    MAX_ATTEMPTS = 5
-
-    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
-        shutil.copyfileobj(file.file, temp_file)
-        temp_file_path = temp_file.name
-    
-    await validate_video(temp_file_path)
-
-    for _ in range(MAX_ATTEMPTS):
-        encoded_screenshot, detr_output = take_screenshot_with_detr(temp_file_path)
-        person_objects = [obj for obj in detr_output if obj['label'] == 'person']
-        if person_objects:
-            highest_confidence = max(person_objects, key=lambda x: x['confidence'])
-            extracted_obj = extract_and_save_obj(encoded_screenshot, highest_confidence['box'])
-            os.remove(temp_file_path)
-            return {
-                "screenshot": encoded_screenshot,
-                "extracted_obj": extracted_obj,
-                "detr_output": detr_output
-            }
-    
-    os.remove(temp_file_path)
-    raise HTTPException(status_code=404, detail="no objects detected")
-
-def take_screenshot_with_detr(file_path: str):
-    duration = get_movie_duration(file_path)
-    screenshot_time = random.uniform(0, duration - 1)
-    image_data = take_screenshot(file_path, screenshot_time)
-    image = Image.open(BytesIO(image_data))
-    detr_output = detectObjects(image)
-    encoded_screenshot = base64.b64encode(image_data).decode('utf-8')
-    return encoded_screenshot, detr_output
 
 def process_image_with_detr(file_stream):
     image_stream = BytesIO(file_stream)
--- app/utils/validation.py
+++ app/utils/validation.py
@@ -1,26 +1,9 @@
 from fastapi import UploadFile, HTTPException
 from PIL import Image
 from io import BytesIO
-import subprocess
-import os
 
 async def validate_image(image_stream: BytesIO):
     try:
         Image.open(image_stream).verify()
     except Exception as e:
         raise HTTPException(status_code=400, detail=f"Invalid image file: {str(e)}")
-
-async def validate_video(temp_file_path):
-    try:
-        result = subprocess.run(
-            ["ffprobe", temp_file_path],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT
-        )
-
-        if result.returncode != 0:
-            os.remove(temp_file_path)
-            raise ValueError("Invalid video file")
-
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=f"Invalid video file: {str(e)}")
